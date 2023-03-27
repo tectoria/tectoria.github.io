47c1c88cc325162834306fd21c26d0eed5c877bd
Kingfrankbob <AJWpcc@gmail.com>
2023-03-27 13:08:06 -0500

    Website should be good, look at Google Sheet iplementation

--- js/plugin.js
+++ js/plugin.js
@@ -2726,7 +2726,7 @@ colors = jQuery.Color.names = {
 	navy: "#000080",
 	olive: "#808000",
 	purple: "#800080",
-	red: "#ff0000",
+	red: "#8c90ff",
 	silver: "#c0c0c0",
 	teal: "#008080",
 	white: "#ffffff",
